b44743b4eee028afb6df54cf10fea1b301b36c60
lllewis <lone.un@gmail.com>
2015-05-05 21:43:51 +0800

    sudoku: [i for i in a if not i in b]
    Signed-off-by: lllewis <lone.un@gmail.com>

--- gadget/sudo.py
+++ gadget/sudo.py
@@ -7,21 +7,24 @@ class deSudoku:
         self.sudoku = sudoku 
         self.full = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     
+    def nuInRow(self, row): 
+        '''find number on row'''
+        return [i for i in self.full if i in self.sudoku[row]]
     def emptyNuInRow(self, row): 
-        print("find empty site on row")
-        unset = []
-        set= [] 
-        for i in range(0, 9):
-            if self.full[self.sudoku[row][i] - 1] == self.sudoku[row][i]:
-                    set.append(self.sudoku[row][i])
-            else:
-                    unset.append(self.full[self.sudoku[row][i]])
+        '''find empty site on row'''
+        return [i for i in self.full if not i in self.sudoku[row]]
+        #set= [] 
+        #for i in range(0, 9):
+        #    if self.full[self.sudoku[row][i] - 1] == self.sudoku[row][i]:
+        #        set.append(self.sudoku[row][i])
+        #print(set)
 
-        print(set)
-        print(unset)
-
+    def nuInRow(self, column): 
+        '''find number on column'''
+        return [i for i in self.full if i in self.sudoku[i-1][column]]
     def emptyNuInColumn(self, column): 
         print("find empty site on column")
+        return [i for i in self.full if not i in self.sudoku[i-1][column]]
 
     def emptyNuInSquare(self, square): 
         print("find empty site on square")
@@ -45,5 +48,6 @@ if __name__ == '__main__':
             [2, 0, 0, 0, 0, 0, 3, 8, 0]
             ]
     de = deSudoku(test)
-    de.emptyNuInRow(0)
+    print de.emptyNuInRow(0)
+    print de.nuInRow(0)
     de.printSudoku()
