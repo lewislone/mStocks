eacd5db6ca8d2c36852d450db3866d339c40acea
lllewis <lone.un@gmail.com>
2015-05-08 00:23:51 +0800

    sudoku: square
    Signed-off-by: lllewis <lone.un@gmail.com>

--- gadget/sudo.py
+++ gadget/sudo.py
@@ -8,7 +8,7 @@ class deSudoku:
         self.full = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     
     def nuInRow(self, row): 
-        '''find number on row'''
+        '''find mark site on row'''
         return [i for i in self.full if i in self.sudoku[row]]
     def emptyNuInRow(self, row): 
         '''find empty site on row'''
@@ -19,15 +19,36 @@ class deSudoku:
         #        set.append(self.sudoku[row][i])
         #print(set)
 
-    def nuInRow(self, column): 
-        '''find number on column'''
-        return [i for i in self.full if i in self.sudoku[i-1][column]]
+    def nuInColumn(self, column): 
+        '''find mark site on column'''
+        set = [] 
+        for i in range(0, 9):
+            if self.full[self.sudoku[i][column] - 1] == self.sudoku[i][column]:
+                set.append(self.sudoku[i][column])
+        return set
+         
     def emptyNuInColumn(self, column): 
-        print("find empty site on column")
-        return [i for i in self.full if not i in self.sudoku[i-1][column]]
+        '''find empty site on column'''
+        set = self.nuInColumn(column)
+        return [i for i in self.full if not i in set]
 
+    def getNuInSquare(self, square): 
+        '''get all number on square
+                     1|2|3
+          square =   4|5|6
+                     7|8|9 
+        '''
+        set = []
+        set += self.sudoku[((square-1)/3)*3][((square%3-1)*3) : (3+(square%3-1)*3)]
+        set += self.sudoku[((square-1)/3)*3+1][((square%3-1)*3) : (3+(square%3-1)*3)]
+        set += self.sudoku[((square-1)/3)*3+2][((square%3-1)*3) : (3+(square%3-1)*3)]
+        return set
     def emptyNuInSquare(self, square): 
-        print("find empty site on square")
+        '''find empty site on square'''
+        return [i for i in self.full if not i in self.getNuInSquare(square)]
+    def nuInSquare(self, square): 
+        '''find mark site on square'''
+        return [i for i in self.full if i in self.getNuInSquare(square)]
 
     def printSudoku(self):
         print('###########################')
@@ -50,4 +71,9 @@ if __name__ == '__main__':
     de = deSudoku(test)
     print de.emptyNuInRow(0)
     print de.nuInRow(0)
+    print de.nuInColumn(0)
+    print de.emptyNuInColumn(0)
+    print de.getNuInSquare(5)
+    print de.nuInSquare(5)
+    print de.emptyNuInSquare(5)
     de.printSudoku()
