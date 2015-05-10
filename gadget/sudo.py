90ebbf682360433c8e90edb4e8e8ee101fa6928b
lllewis <lone.un@gmail.com>
2015-05-10 16:19:22 +0800

    sudoku: start one rule
    Signed-off-by: lllewis <lone.un@gmail.com>

--- gadget/sudo.py
+++ gadget/sudo.py
@@ -7,6 +7,12 @@ class deSudoku:
         self.sudoku = sudoku 
         self.full = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     
+    def printSudoku(self):
+        print('###########################')
+        for i in range(0, 9):
+            print(self.sudoku[i])
+        print('###########################\n')
+
     def nuInRow(self, row): 
         '''find mark site on row'''
         return [i for i in self.full if i in self.sudoku[row]]
@@ -39,22 +45,50 @@ class deSudoku:
                      7|8|9 
         '''
         set = []
-        set += self.sudoku[((square-1)/3)*3][((square%3-1)*3) : (3+(square%3-1)*3)]
-        set += self.sudoku[((square-1)/3)*3+1][((square%3-1)*3) : (3+(square%3-1)*3)]
-        set += self.sudoku[((square-1)/3)*3+2][((square%3-1)*3) : (3+(square%3-1)*3)]
+        set += self.sudoku[((square-1)/3)*3][((square%3)*2) : (3+(square%3)*2)]
+        set += self.sudoku[((square-1)/3)*3+1][((square%3)*2) : (3+(square%3)*2)]
+        set += self.sudoku[((square-1)/3)*3+2][((square%3)*2) : (3+(square%3)*2)]
         return set
     def emptyNuInSquare(self, square): 
         '''find empty site on square'''
         return [i for i in self.full if not i in self.getNuInSquare(square)]
-    def nuInSquare(self, square): 
+    def nuInSquare(self, square):
         '''find mark site on square'''
         return [i for i in self.full if i in self.getNuInSquare(square)]
 
-    def printSudoku(self):
-        print('###########################')
-        for i in range(0, 9):
-            print(self.sudoku[i])
-        print('###########################\n')
+    def whichSquare(self, pos):
+        return (pos[0]/3*3 + pos[1]/3 + 1) 
+
+    #rule number 1
+    def ruleCheckRowColumnSquare(self, pos):
+        row = pos[0]
+        column = pos[1]
+        square = self.whichSquare([row, column])
+        rowChance = self.emptyNuInRow(row)
+        columnSet = self.nuInColumn(column)
+        squareSet = self.nuInSquare(square)
+        print rowChance
+        print columnSet
+        print squareSet
+        print pos 
+        print square 
+        for i in columnSet:
+            if i in rowChance:
+                rowChance.remove(i)
+        for i in squareSet:
+            if i in rowChance:
+                rowChance.remove(i)
+        if len(rowChance) == 1:
+            self.sudoku[row][column] = rowChance[0]
+            self.printSudoku()
+        elif len(rowChance) == 0:
+            print '#ERR#'
+        else:
+            return
+        
+    #rule number 2
+    def ruleOnlyOneNuInLine(self):
+        '''every line/column has one "9"'''
 
 if __name__ == '__main__':
     test = [
@@ -69,11 +103,10 @@ if __name__ == '__main__':
             [2, 0, 0, 0, 0, 0, 3, 8, 0]
             ]
     de = deSudoku(test)
-    print de.emptyNuInRow(0)
-    print de.nuInRow(0)
-    print de.nuInColumn(0)
-    print de.emptyNuInColumn(0)
-    print de.getNuInSquare(5)
-    print de.nuInSquare(5)
-    print de.emptyNuInSquare(5)
+
     de.printSudoku()
+    for i in range(0, 9):
+        for j in range(0, 9):
+            if test[i][j] == 0:
+                de.ruleCheckRowColumnSquare([i, j])
+    
