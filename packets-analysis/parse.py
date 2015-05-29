6537b1da5742dcd30eb74be3beff9495a9aee153
lllewis <lone.un@gmail.com>
2015-05-30 05:16:03 +0800

    packets-analysis: parase pcap file and store infos in json file
    Signed-off-by: lllewis <lone.un@gmail.com>

--- packets-analysis/parse.py
+++ packets-analysis/parse.py
@@ -8,19 +8,70 @@ import hashlib
 import StringIO
 #from matplotlib.pyplot import *
 
-class PARSE_PCAP:
+class TCP_STREAM:
         def __init__(self):
-            '''init some parameters'''
-            self.streams = []
+            '''stream init...
+               streams= {
+                         'id':{
+                            'c_ip':
+                            's_ip':
+                            'c_port':
+                            's_port':
+                            'c_packets':[],
+                            's_packets':[],
+                            'data_len':,
+                            'statt_time':,
+                            'end_time':},
+                        'id':{...},
+                        ...,
+                        'id':{...}}
+            '''
+            self.streams = {} 
+        def add(self, ts, ippkt, tcppkt):
+            if isinstance(tcppkt, dpkt.tcp.TCP): #TCP
+                    dst = '%d.%d.%d.%d' % tuple(map(ord, list(ippkt.dst)))
+                    src = '%d.%d.%d.%d' % tuple(map(ord, list(ippkt.src)))
+                    des = src + ":" + str(tcppkt.sport) + " -> " + dst + ":" + str(tcppkt.dport)
+                    des_revert = dst + ":" + str(tcppkt.dport) + " -> " + src + ":" + str(tcppkt.sport)
+                    packet = {}
+                    packet['ts'] = ts
+                    packet['src'] = src
+                    packet['dst'] = dst
+                    packet['id'] = ippkt.id
+                    packet['sport'] = tcppkt.sport
+                    packet['dport'] = tcppkt.dport
+                    packet['flags'] = tcppkt.flags
+                    packet['seq'] = tcppkt.seq
+                    packet['ack'] = tcppkt.ack
+                    packet['win'] = tcppkt.win 
+                    stream_id = hashlib.md5(des).hexdigest()
+                    stream_id_revert = hashlib.md5(des_revert).hexdigest()
 
-        def search_stream(self, streams, id):
-            for stream in streams:
-                if stream['stream_id'] == id:
-                   return stream
+                    if stream_id in self.streams:
+                        self.streams[stream_id]['c_packets'].append(packet)
+                        self.streams[stream_id]['data_len'] += len(tcppkt.data)
+                    elif stream_id_revert in self.streams:
+                        self.streams[stream_id_revert]['s_packets'].append(packet)
+                        self.streams[stream_id_revert]['data_len'] += len(tcppkt.data)
+                    else:#new
+                        self.streams[stream_id] = {} 
+                        self.streams[stream_id]['c_ip'] = src
+                        self.streams[stream_id]['s_ip'] = dst
+                        self.streams[stream_id]['c_port'] = tcppkt.sport 
+                        self.streams[stream_id]['s_port'] = tcppkt.dport 
+                        self.streams[stream_id]['data_len'] = 0 
+                        self.streams[stream_id]['start_time'] = ts
+                        self.streams[stream_id]['c_packets'] = []
+                        self.streams[stream_id]['s_packets'] = []
+                        self.streams[stream_id]['c_packets'].append(packet)
 
-            return False 
+class PCAP_PARSE:
+        def __init__(self):
+            '''parse pcap init some parameters
+            '''
+            self.stream = TCP_STREAM()
 
-        def collect_tcp(self, pcap):
+        def collect_streams(self, pcap):
             reader = dpkt.pcap.Reader(pcap)
 
             #Determine the packet type.
@@ -40,62 +91,33 @@ class PARSE_PCAP:
               print 'unknown packet type!!' 
               return
 
-            print reader
-            return
-            for ts, buf in pcap:
-                new = 0;
-                try:
-                        eth = dpkt.ethernet.Ethernet(buf)
-                        #print ts
-
-                        ##ssl: capture by "--any" option
-                        if eth.data.__class__.__name__=='str':
-                            eth = dpkt.ethernet.Ethernet(buf[2:])
-                        
-                        if eth.data.__class__.__name__=='IP':
-                            dst = '%d.%d.%d.%d' % tuple(map(ord, list(eth.data.dst)))
-                            src = '%d.%d.%d.%d' % tuple(map(ord, list(eth.data.src)))
-                            id = eth.data.id 
-                            #ip = dpkt.ip.IP(eth.data)
-                            #print src + " -> " + dst 
-                except Exception:
-                    print 'ip exception'
-                    continue
+            for ts, buf in reader:
+                if PacketClass:
+                    packet = PacketClass(buf)
+                    ippkt = packet.data
+                else:
+                    packet = dpkt.ip.IP(buf)
+                    ippkt = packet
+                #dst = '%d.%d.%d.%d' % tuple(map(ord, list(ippkt.dst)))
+                #src = '%d.%d.%d.%d' % tuple(map(ord, list(ippkt.src)))
+                #print str(ts) + ": " + src + " -> " + dst 
 
                 try:
-                        if eth.data.data.__class__.__name__=='TCP':
-                            #tcp = dpkt.tcp.TCP(eth.data.data)
-                            tcp = eth.data.data
-                            des = src + ":" + str(tcp.sport) + " -> " + dst + ":" + str(tcp.dport)
-                            #print des
-                            stream_id = hashlib.md5(des).hexdigest()
-                            stream = self.search_stream(streams, stream_id)
-                            if stream is False:
-                                stream = {}
-                                stream['stream_id'] = stream_id 
-                                stream['id'] = id 
-                                stream['src'] = src
-                                stream['dst'] = dst
-                                stream['sport'] = tcp.sport
-                                stream['dport'] = tcp.dport
-                                stream['flags'] = tcp.flags
-                                stream['seq'] = [] 
-                                stream['ack'] = [] 
-                                stream['win'] = [] 
-                                new = 1
-                            stream['win'].append((ts, tcp.win))
-                            stream['seq'].append((ts, tcp.seq))
-                            stream['ack'].append((ts, tcp.ack))
-                except Exception:
-                    print 'tcp exception'
-                    continue
+                    if isinstance(ippkt, dpkt.ip.IP):
+                        if isinstance(ippkt.data, dpkt.tcp.TCP): #TCP
+                            tcppkt = ippkt.data
+                            self.stream.add(ts, ippkt, tcppkt)
+                        else:
+                            print("it is not a tcp packet!!!")
+                            continue
+                    else:
+                        print("it is not a ip packet!!!")
+                        continue
+                except dpkt.Error, error:
+                        print(error)
+                        return
 
-                if new == 1:
-                    streams.append(stream)
-
-            return streams
-
-parse = PARSE_PCAP()
+parse = PCAP_PARSE()
 
 if __name__ == '__main__':
     cap_file = sys.argv[1]
@@ -105,91 +127,89 @@ if __name__ == '__main__':
 
     #f = file(cap_file)
     f = file("/home/lewis/work/tmp/xx.cap")
-    streams = parse.collect_tcp(f)
-    exit()
-
+    parse.collect_streams(f)
+    print parse.stream.streams 
 
     file = open('./stream.json', "ab")
-    a = {'streams': streams}
-    json.dump(streams, file)
+    json.dump(parse.stream.streams, file)
 
-    stream = {}
-    for stream in streams:
-        time1 = [seq[0] for seq in stream['seq']]
-        seq = [seq[1] for seq in stream['seq']]
+   # stream = {}
+   # for stream in streams:
+   #     time1 = [seq[0] for seq in stream['seq']]
+   #     seq = [seq[1] for seq in stream['seq']]
 
-        if len(seq) < 10:
-            continue
+   #     if len(seq) < 10:
+   #         continue
 
-        des1 = stream['src'] + ":" + str(stream['sport']) + " -> " + stream['dst'] + ":" + str(stream['dport'])
-        des2 = stream['dst'] + ":" + str(stream['dport']) + " -> " + stream['src'] + ":" + str(stream['sport'])
-        stream_id = hashlib.md5(des2).hexdigest()
-        print stream['stream_id']
-        stream = parse.search_stream(streams, stream_id)
-        if stream == False:
-            continue
-        time2 = [ack[0] for ack in stream['ack']]
-        ack = [ack[1] for ack in stream['ack']]
-        win = [win[1] for win in stream['win']]
+   #     des1 = stream['src'] + ":" + str(stream['sport']) + " -> " + stream['dst'] + ":" + str(stream['dport'])
+   #     des2 = stream['dst'] + ":" + str(stream['dport']) + " -> " + stream['src'] + ":" + str(stream['sport'])
+   #     stream_id = hashlib.md5(des2).hexdigest()
+   #     print stream['stream_id']
+   #     stream = parse.search_stream(streams, stream_id)
+   #     if stream == False:
+   #         continue
+   #     time2 = [ack[0] for ack in stream['ack']]
+   #     ack = [ack[1] for ack in stream['ack']]
+   #     win = [win[1] for win in stream['win']]
 
-        ##remove zero value
-        if seq[0] == 0:
-            seq[0] = seq[1]
-            time1[0] = time1[1]
-        if ack[0] == 0:
-            ack[0] = ack[1]
-            time2[0] = time2[1]
+   #     ##remove zero value
+   #     if seq[0] == 0:
+   #         seq[0] = seq[1]
+   #         time1[0] = time1[1]
+   #     if ack[0] == 0:
+   #         ack[0] = ack[1]
+   #         time2[0] = time2[1]
 
-        ##skip server(only send data) direction package
-        if ack[1] == ack[len(ack)/2]:
-            continue
+   #     ##skip server(only send data) direction package
+   #     if ack[1] == ack[len(ack)/2]:
+   #         continue
 
-        ##outstanding bytes
-        i = 0
-        send = 0
-        acked = 0
-        flight = []
-        while i < len(seq):
-            if send < seq[i]:
-                send = seq[i] 
+   #     ##outstanding bytes
+   #     i = 0
+   #     send = 0
+   #     acked = 0
+   #     flight = []
+   #     while i < len(seq):
+   #         if send < seq[i]:
+   #             send = seq[i] 
 
-            j = 0 
-            for time in time2:
-                if time > time1[i]:
-                   break
-                j = j + 1
-            
-            if j == len(time2):
-                j = j - 1
-            if acked < ack[j]:
-                acked = ack[j]
-            flight.append(send - acked) 
-            i = i + 1 
+   #         j = 0 
+   #         for time in time2:
+   #             if time > time1[i]:
+   #                break
+   #             j = j + 1
+   #         
+   #         if j == len(time2):
+   #             j = j - 1
+   #         if acked < ack[j]:
+   #             acked = ack[j]
+   #         flight.append(send - acked) 
+   #         i = i + 1 
 
-       # ##sent/ack 
-       # grid(True)
-       # ylabel('seqno', fontdict={'fontsize':20})
-       # xlabel('time(s)', fontdict={'fontsize':20})
-       # plot(time1, seq, 'ro:', linewidth=2, label='sent ' + des1)
-       # plot(time2, ack, 'go:', linewidth=2, label='acked ' + des2)
-       # legend(title=cap_file, loc='best')
-       # #savefig('ack_timestamp.png')
-       # show()
+   #    # ##sent/ack 
+   #    # grid(True)
+   #    # ylabel('seqno', fontdict={'fontsize':20})
+   #    # xlabel('time(s)', fontdict={'fontsize':20})
+   #    # plot(time1, seq, 'ro:', linewidth=2, label='sent ' + des1)
+   #    # plot(time2, ack, 'go:', linewidth=2, label='acked ' + des2)
+   #    # legend(title=cap_file, loc='best')
+   #    # #savefig('ack_timestamp.png')
+   #    # show()
 
-       # ##receive windows
-       # grid(True)
-       # ylabel('windows', fontdict={'fontsize':20})
-       # xlabel('time(s)', fontdict={'fontsize':20})
-       # plot(time2, win, 'y-o', linewidth=2, label='win time(s) ' + des1)
-       # legend(title=cap_file +'(windows)', loc='best')
-       # #savefig('ack_timestamp.png')
-       # show()
+   #    # ##receive windows
+   #    # grid(True)
+   #    # ylabel('windows', fontdict={'fontsize':20})
+   #    # xlabel('time(s)', fontdict={'fontsize':20})
+   #    # plot(time2, win, 'y-o', linewidth=2, label='win time(s) ' + des1)
+   #    # legend(title=cap_file +'(windows)', loc='best')
+   #    # #savefig('ack_timestamp.png')
+   #    # show()
 
-       # ##outstanding bytes
-       # grid(True)
-       # ylabel('outstanding', fontdict={'fontsize':20})
-       # xlabel('time(s)', fontdict={'fontsize':20})
-       # plot(time1, flight, 'y-o', linewidth=2, label='outstanding time(s) ' + des1)
-       # legend(title=cap_file+ '(outstanding)', loc='best')
-       # #savefig('ack_timestamp.png')
-       # show()
+   #    # ##outstanding bytes
+   #    # grid(True)
+   #    # ylabel('outstanding', fontdict={'fontsize':20})
+   #    # xlabel('time(s)', fontdict={'fontsize':20})
+   #    # plot(time1, flight, 'y-o', linewidth=2, label='outstanding time(s) ' + des1)
+   #    # legend(title=cap_file+ '(outstanding)', loc='best')
+   #    # #savefig('ack_timestamp.png')
+   #    # show()
